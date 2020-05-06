fa8af8762b089a6ef6537ce061a1f13158cc3342
Tobias Hoepfner <Tobias.Hoepfner@arbeitsagentur.de>
2020-05-06 09:11:10 +0200

    now it shows something

---
+++ finance-guru/precache-manifest.ce01ede22fdce638438f51a5f21b4285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21decad691680c059eb39511f9984e5a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3be81cf3a248b4d1477c",
+    "url": "./static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "a4089d8d9b74be665930",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3be81cf3a248b4d1477c",
+    "url": "./static/js/2.ca79d3c6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.ca79d3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4089d8d9b74be665930",
+    "url": "./static/js/main.e3367150.chunk.js"
+  },
+  {
+    "revision": "8e5a282cc0e41e752b74",
+    "url": "./static/js/runtime-main.1436d014.js"
+  }
+]);
